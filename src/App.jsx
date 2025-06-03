b87f47a23d4524f19ba99e9a56630ca0e0158db4
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-06-03 16:04:11 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='underline text-3x1'>App</h1>
+    </div>
+  )
+}
+
+export default App
