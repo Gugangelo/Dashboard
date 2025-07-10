7b7995aa61ae1596404ab2d96a71e63dddf1945a
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-07-10 16:28:43 -0300

    feat: Nova aba clientes

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnDirective, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject, Toolbar, Search } from '@syncfusion/ej2-react-grids'
+import { customersData, customersGrid } from '../data/dummy'
+import { Header } from '../components' 
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Página' title="Clientes"/>
+      <GridComponent
+        dataSource={customersData}
+        allowPaging
+        allowSorting
+        toolbar={["Search", "Delete"]}
+        editSettings={{ allowDeleting: true, allowEditing: true }}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {customersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject services={[Page, Toolbar, Search, Sort, Filter, Edit]} />
+      </GridComponent>
+    </div>
   )
 }
 
