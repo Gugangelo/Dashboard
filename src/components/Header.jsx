baf00eb4fd00d73794de60b67b1ee09f3b636f19
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-07-13 23:35:08 -0300

    feat: Componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({ category, title }) => {
   return (
-    <div>Header</div>
+    <div className='mb-10'>
+      <p className='text-gray-400 dark:text-gray-100'>
+        {category}
+      </p>
+      <p className='text-3xl font-extrabold tracking-tight text-slate-900 dark:text-slate-100'>{title}</p>
+    </div>
   )
 }
 
