4b535e8681d9f1a2bcdf00c74cd7282bbf9b8460
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-07-18 13:03:07 -0300

    feat: Cores de tema adicionadas

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -6,8 +6,10 @@ import { earningData, SparklineAreaData, ecomPieChartData } from '../data/dummy'
 import { useStateContext } from '../contexts/ContextProvider'
 
 const Ecommerce = () => {
+  const { currentColor } = useStateContext()
+
   return (
-    <div className='mt-12'>
+    <div className='mt-24'>
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
         <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-no-repeat bg-cover bg-center'>
 
@@ -16,14 +18,22 @@ const Ecommerce = () => {
               <p className='font-bold text-gray-400'>Ganhos</p>
               <p className='text-2xl'>R$63.448,78</p>
             </div>
+            <button
+              type="button"
+              style={{ backgroundColor: currentColor }}
+              className="text-2xl opacity-0.9 text-white hover:drop-shadow-xl rounded-full  p-4"
+            >
+              <BsCurrencyDollar />
+            </button>
           </div>
 
           <div className='mt-6'>
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
+              size="md"
             />
           </div>
         </div>
@@ -50,7 +60,7 @@ const Ecommerce = () => {
         </div>
 
         <div className='flex gap-10 flex-wrap justify-center'>
-          <div className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 rounded-2xl md:w-780'>
+          <div className='p-4 bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 rounded-2xl md:w-780'>
             <div className='flex justify-between'>
               <p className='font-semibold text-xl'>Atualizações de Receita</p>
               <div className='flex items-center gap-4'>
@@ -81,18 +91,30 @@ const Ecommerce = () => {
                   <p className='text-gray-500 mt-1'>Despesa</p>
                 </div>
                 
-                <div className='mt-5 bg-gray-100'>
+                <div className='mt-5'>
                   <SparkLine 
-                    currentColor="blue"
+                    currentColor={currentColor}
                     id="line-sparkline"
                     type="Line"
                     height="80px"
                     width="250px"
                     data={SparklineAreaData}
-                    color="blue"
-                    loaded={() => console.log('Sparkline carregado')}
+                    color={currentColor}
                   />  
                 </div>
+                <div className='mt-10'>
+                  <Button 
+                    color="white"
+                    bgColor={currentColor}
+                    text="Baixar Relatório"
+                    borderRadius="10px"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Stacked width="320px"
+                  height="360px" />
               </div>
             </div>
           </div>
