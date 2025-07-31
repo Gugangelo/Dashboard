0df60c6d1ccd95f0c9408ab282283446fa0eac4d
Gustavo Angelo <78031691+Gugangelo@users.noreply.github.com>
2025-07-31 16:56:15 -0300

    feat: Theme Settings

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,18 @@ import { useStateContext } from './contexts/ContextProvider'
 import './App.css'
 
 function App() {
-  const { activeMenu } = useStateContext()
+  const { activeMenu, themeSettings, setThemeSettings, currentColor, currentMode } = useStateContext()
 
   return (
-    <div>
+    <div className={currentMode === 'Dark' ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
             <TooltipComponent content='Settings' position='Top'>
               <button type='button' 
                 className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                style={{ background: 'blue', borderRadius: '25%' }}
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: '25%' }}
               >
                 <FiSettings style={{ color: 'white' }} />
               </button>
@@ -35,8 +36,10 @@ function App() {
             </div>
           )}
 
-          <div className={`dark:bg-main-bg bg-main-bg min-h-screen w-full 
-            ${activeMenu ? 'md:ml-72' : 'flex-2'}`}>
+          <div className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+            ${activeMenu 
+              ? 'md:ml-72' 
+              : 'flex-2'}`}>
             <div 
               className='fixed md:static bg-main-bg dark:bg-main-dark-bg w-full navbar'
             >
@@ -44,6 +47,8 @@ function App() {
             </div>
 
             <div>
+              {themeSettings && <ThemeSettings />}
+
               <Routes>
                 {/* Dashboard */}
                 <Route path='/' />
